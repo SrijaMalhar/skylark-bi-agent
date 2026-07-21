ff106ce81a33e7662f24eb85d503ca7bd10c184d
Srija Malhar <srijamalhar@gmail.com>
2026-07-21 15:52:00 +0530

    add comment about board id format

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 # Edit these two values before running
+# Find the board ID in your Monday.com URL: /boards/<ID>
 WORK_ORDERS_BOARD_ID = "YOUR_WORK_ORDERS_BOARD_ID"
 DEALS_BOARD_ID = "YOUR_DEALS_BOARD_ID"
 
